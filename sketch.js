10ba5d67d911114939744dddddba0c1041407b89
RocketGuy569 <114612012+RocketGuy569@users.noreply.github.com>
2022-10-10 19:31:09 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+i=0
+for (i=0;i<marks.length;i++) {
+  if (marks[i]>=45) {
+    console.log(marks[i])
+  }
+}
+console.log(i)
+console.log(marks[i]);
+i++
+console.log(i);
+console.log(marks[i]);
+console.log(marks.length)
+function setup() 
+{
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
